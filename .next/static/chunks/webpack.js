c53e3bc0697f15f6b0eab486fabb7e892b26033b
offor <offorchukwuebuka11@gmail.com>
2025-04-28 11:10:10 +0100

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "333c14030fe67fc6"; }
+/******/ 		__webpack_require__.h = function() { return "d0535cc925104687"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
