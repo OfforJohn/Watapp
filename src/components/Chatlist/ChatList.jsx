9db149cfb46b845265e7cfd0bd560c50f63e44da
offor <offorchukwuebuka11@gmail.com>
2025-05-24 12:28:42 +0100

    Cl

--- src/components/Chatlist/ChatList.jsx
+++ src/components/Chatlist/ChatList.jsx
@@ -4,7 +4,7 @@ import List from "./List";
 import SearchBar from "./SearchBar";
 import ContactsList from "./ContactsList";
 import { useStateProvider } from "@/context/StateContext";
-
+import Avatar from "../common/Avatar";
 import { BsChatDots } from "react-icons/bs";
 import { CiSettings } from "react-icons/ci";
 import { LuCircleDotDashed } from "react-icons/lu";
@@ -13,6 +13,8 @@ import { LuCircleDotDashed } from "react-icons/lu";
 
 export default function ChatList() {
   const [pageType, setPageType] = useState("default");
+  
+  const [{ userInfo }, dispatch] = useStateProvider();
   const [{ contactsPage }] = useStateProvider();
   const [navOpen, setNavOpen] = useState(false);
 
@@ -56,21 +58,27 @@ export default function ChatList() {
     {navOpen && <span className="whitespace-nowrap">Chats</span>}
   </button>
 
-<button className="flex items-center space-x-2 p-2 mt-4 hover:text-emerald-400">
+<button className="flex items-center ml-[-1%] space-x-2 p-2 mt-4 hover:text-emerald-400">
   <LuCircleDotDashed size={24} className="text-blue-500" />
   {navOpen && <span className="whitespace-nowrap">Bots</span>}
 </button>
 
 
-  
- 
-  {/* Spacer to push Settings to bottom */}
-  <div className="mt-auto">
-    <button className="flex items-center space-x-2 p-2 hover:text-emerald-400">
-      <CiSettings size={20} />
+   <div className="mt-80">
+    <button className="flex items-center ml-[-2%] space-x-2 p-2 hover:text-emerald-400">
+      <CiSettings size={33} />
       {navOpen && <span className="whitespace-nowrap">Settings</span>}
     </button>
   </div>
+  <div className="mt-auto">
+     <button className="flex items-center ml-[-4%] space-x-2 p-2 hover:text-emerald-400">
+     
+        <Avatar type="sm" image={userInfo?.profileImage} />
+      {navOpen && <span className="whitespace-nowrap">Profile</span>}
+       </button>
+  </div>
+  {/* Spacer to push Settings to bottom */}
+ 
 </div>
 
       
