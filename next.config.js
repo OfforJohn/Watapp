5303f998a085ab4358d2c3f4ad832094d4eb2fda
John <synergypromax@gmail.com>
2026-01-27 22:35:38 +0100

    addedimages

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   },
   reactStrictMode: false,
   images: {
-    domains: ["render-backend-ksnp.onrender.com"],
+    domains: ["render-backend1-a38s.onrender.com"],
   },
 };
 
