884364ddf542a27b94cf6bb3dd77959801ddbe52
John <147444612+OfforJohn@users.noreply.github.com>
2026-05-06 05:35:17 -0700

    done

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -72,15 +72,17 @@ export default function Login() {
   };
 
   return (
-    <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
-      <div className="flex items-center justify-center gap-2 text-white">
-        <Image
-          src="https://cdn.discordapp.com/attachments/1364573740406411295/1501331269219520512/IMG_4024.gif?ex=69fc57f3&is=69fb0673&hm=f4b0c7255b29e1370432355f7dbd7d543a1ad95f494323aca6c091b36930c03c&"
-          alt="whatsapp-gif"
-          height={300}
-          width={300}
+    <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-2">
+      <div className="flex items-center justify-center text-white">
+        <video
+          src="/dcddc.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+          style={{ height: 450, width: 450, mixBlendMode: 'screen', filter: 'contrast(1.1) brightness(1.1)' }}
         />
-        <span className="text-7xl">WhatsApp</span>
+        <span className="text-7xl -ml-8">WhatsApp</span>
       </div>
       <button
         className="flex items-center justify-center gap-7 bg-search-input-container-background p-5 rounded-lg"
--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -88,14 +88,16 @@ export default function OnBoarding() {
 
   return (
     <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
-      <div className="flex items-center justify-center gap-2">
-        <Image
-          src="https://cdn.discordapp.com/attachments/1364573740406411295/1501331269219520512/IMG_4024.gif?ex=69fc57f3&is=69fb0673&hm=f4b0c7255b29e1370432355f7dbd7d543a1ad95f494323aca6c091b36930c03c&"
-          alt="whatsapp-gif"
-          height={300}
-          width={300}
+      <div className="flex items-center justify-center">
+        <video
+          src="/dcddc.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+          style={{ height: 450, width: 450, mixBlendMode: 'screen', filter: 'contrast(1.1) brightness(1.1)' }}
         />
-        <span className="text-7xl">WhatsApp</span>
+        <span className="text-7xl -ml-8">WhatsApp</span>
       </div>
       <div></div>
       <h2 className="text-2xl ">Create your profile</h2>
